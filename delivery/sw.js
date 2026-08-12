bfaa997a0b5cbc396b52e0f21617ada97e7d62b1
fabioikoma <fabio.ikoma@gmail.com>
2026-08-12 11:59:29 -0300

    Update service worker cache version to v3

--- delivery/sw.js
+++ delivery/sw.js
@@ -1,4 +1,4 @@
-const CACHE='cdm-delivery-v2';
+const CACHE='cdm-delivery-v3';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil((async()=>{
   const ks=await caches.keys();
@@ -9,7 +9,7 @@ self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
   const u=new URL(e.request.url);
   // HTML e dados: sempre rede primeiro (nunca serve HTML velho)
-  e.respondWith(fetch(e.request).then(r=>{
+  e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{
     try{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp))}catch(_){}
     return r;
   }).catch(()=>caches.match(e.request)));
